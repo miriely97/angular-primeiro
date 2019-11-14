3682cef2b02f4ff7de9fc5f575e37131d7c3e21a
miriely97 <carlamiriely@gmail.com>
2019-11-14 10:29:11 -0300

    3 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, CabecalhoComponent, ConteudoComponent, RodapeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'hello',
+  selector: 'carla',
   template: `<h1>Hello {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`]
 })
